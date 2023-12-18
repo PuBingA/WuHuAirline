48eddecd7cda075dc48ec81d2fdd0b3c96d68d40
Monaxia <115874554+monaxiastr@users.noreply.github.com>
2023-12-18 14:00:14 +0800

    选图功能
    编辑define内容到public define中

--- Classes/map.h
+++ Classes/map.h
@@ -10,24 +10,15 @@ public:
     static cocos2d::Scene* createScene();
 
     virtual bool init();//创建场景类,被继承公有部分
-
     virtual void input_background() {};//放置背景图
-
     virtual void input_walk_way() {};//放置怪物行进路线
-
     void menuCallback(cocos2d::Ref* pSender);//菜单响应函数
-
     void input_return_pause();//放置返回，以及暂停键   暂停键未实现
 
     // implement the "static create()" method manually
     CREATE_FUNC(Map_father);
-
-
 };
 
-
-#define walk_way_begin_1 {250,550} //怪物开始移动的坐标
-#define way_size 82//每个地板的像素大小
 class Map_One :public Map_father
 {
 public:
@@ -39,8 +30,6 @@ private:
     std::vector<std::vector<double>>walk_way_store_1;//存放怪物行进的x,y轴的向量
 };
 
-
-#define walk_way_begin_2 {250-way_size,550-5*way_size} //怪物开始移动的坐标
 class Map_Two :public Map_father
 {
 public:
@@ -52,7 +41,6 @@ private:
     std::vector<std::vector<double>>walk_way_store_2;//存放怪物行进的x,y轴的向量
 };
 
-#define walk_way_begin_3 {250,600} //怪物开始移动坐标
 class Map_Three :public Map_father
 {
 public:
--- Classes/public_define.h
+++ Classes/public_define.h
@@ -4,5 +4,9 @@
 #define return_x  1170     
 #define return_y  630    //返回选择关卡界面坐标
 
+#define way_size 82//每个地板的像素大小
+#define walk_way_begin_1 {250,550} //怪物开始移动的坐标
+#define walk_way_begin_2 {250-way_size,550-5*way_size} //怪物开始移动的坐标
+#define walk_way_begin_3 {250,600} //怪物开始移动坐标
 
 
